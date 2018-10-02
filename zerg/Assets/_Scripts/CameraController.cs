c8012b03c7a04079411d83cde769b5cc21e095b3
alexjjia <alexjjia@gmail.com>
2018-10-01 20:59:55 -0700

    Init commit, and added simple camera panning and zooming based off mouse position and WASD (to pan the camera), and the mouse scroll (to zoom in and out).

---
+++ zerg/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private float panSpeed = 10f;
+    private float panBorderThickness = 10f;
+    private float zoom;
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = transform.position;
+
+        //Moving camera upwards.
+        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            position.z += panSpeed * Time.deltaTime;
+        }
+        //Moving camera leftwards.
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            position.x -= panSpeed * Time.deltaTime;
+        }
+        //Moving camera downwards.
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            position.z -= panSpeed * Time.deltaTime;
+        }
+        //Moving camera rightwards.
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            position.x += panSpeed * Time.deltaTime;
+        }
+        //Zooming camera inwards.
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            position.y -= panSpeed * Time.deltaTime;
+        }
+        //Zooming camera outwards.
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            position.y += panSpeed * Time.deltaTime;
+        }
+
+        transform.position = position;
+		
+	}
+}
